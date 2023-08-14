70fd1d9ab80d8e1aa1e475c881eaf0755fed39b7
manolo hernandez rojas <manoloh782@gmail.xomgit config --global user.email>
2023-08-14 13:29:58 -0500

    todo

---
+++ Taller1_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+/**
+ * 
+ */
+public class Modificacion {
+
+	/**
+	 * 
+	 */
+	public Modificacion() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hola, mundo!");
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+
+	}
+		
+
+}
